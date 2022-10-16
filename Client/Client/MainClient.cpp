29bda202f37edbddfcd3d3151fac9bfb40052daa
Stefanos Triantafyllidis <sttqueries@gmail.com>
2022-10-17 00:07:28 +0100

    Revert "Revert "Revert "Put everything so far in Classes"""
    This reverts commit 4e71dc2de2a4f31d099b0766f50d7962bcd987b0.

--- Client/Client/MainClient.cpp
+++ Client/Client/MainClient.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <SDL.h>
-#include "TCPClient.h"
+#include <SDL_net.h>
+
+IPaddress ip;
+const int port = 1234;
 int main(int argc, char* argv[])
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
@@ -9,9 +12,43 @@ int main(int argc, char* argv[])
 		system("pause");
 		return 0;
 	}
-	TCPClient clientConnection;
-	clientConnection.Connect();
 
+	else if (SDLNet_Init() == 1)
+	{
+		std::cout << "SDL net could not initialise" << std::endl;
+		system("pause");
+		return 0;
+	}
+
+	if (SDLNet_ResolveHost(&ip, "localhost", port) == -1)
+	{
+		std::cout << "could not establish connection with server" << std::endl;
+		return 0;
+	}
+	TCPsocket socket = SDLNet_TCP_Open(&ip);
+	if (!socket)
+	{
+		std::cout << "could not connect to server" << std::endl;
+		return 0;
+	}
+	else
+	{
+		std::cout << "Connection to server successful" << std::endl;
+	}
+	char message[2000];
+	//receive a message from the client and store
+	if (SDLNet_TCP_Recv(socket, message, 2000) <= 0)
+	{
+		std::cout << "message could not be received" << std::endl;
+	}
+	else
+	{
+		std::cout << "message from " << socket << ": " << message << std::endl;
+	}
+
+
+	SDLNet_Quit();
 	SDL_Quit();
+	system("pause");
 	return 0;
 }
--- Host/Host/MainHost.cpp
+++ Host/Host/MainHost.cpp
@@ -1,6 +1,9 @@
-#include <SDL.h>
 #include <iostream>
-#include "TCPHost.h"
+#include <SDL.h>
+#include <SDL_net.h>
+
+IPaddress ip;
+const int port = 1234;
 int main(int argc, char* argv[])
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
@@ -9,8 +12,48 @@ int main(int argc, char* argv[])
 		system("pause");
 		return 0;
 	}
-	TCPHost HostConnection;
-	HostConnection.WelcomeMessage("Howdy doo");
+
+	else if (SDLNet_Init() == 1)
+	{
+		std::cout << "SDL net could not initialise" << std::endl;
+		system("pause");
+	}
+	if (SDLNet_ResolveHost(&ip, nullptr, 1234) == -1)
+	{
+		//could not create a server
+	}
+	TCPsocket listenSocket = SDLNet_TCP_Open(&ip);
+	if (!listenSocket)
+	{
+		std::cout << "server socket closed for clients" << std::endl;
+	}
+	else
+	{
+		std::cout << "server socket open for clients" << std::endl;
+	}
+	TCPsocket clientSocket = 0;
+	while (!clientSocket)
+	{
+		clientSocket = SDLNet_TCP_Accept(listenSocket);
+		std::cout << "." << std::endl;
+		SDL_Delay(500u);
+	}
+	//create a message to be sent to the client
+	std::string message = "Hello and welcome to the chat!";
+	//store length of message for use below
+	int length = message.length() + 1;
+	//send the above message to the client via the socket
+	if (SDLNet_TCP_Send(clientSocket, message.c_str(), length) < length)
+	{
+		std::cout << "message not sent" << std::endl;
+	}
+	else
+	{
+		std::cout << "message sent" << std::endl;
+	}
+	system("pause");
+	SDLNet_TCP_Close(listenSocket);
+	SDLNet_Quit();
 	SDL_Quit();
 	return 0;
 }
